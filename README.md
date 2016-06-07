1bfb36d568479bcb9312363ebc034e3b8e6f72ec
Adam S <asa188@sfu.ca>
2016-06-07 11:41:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Dummy Certification Authority
+A quick and dirty way for creating a Root Certification Authority (CA) and one Intermediate CA for testing.
+
+## Build
+```
+git clone https://github.com/kintoandar/dummy_ca.git
+cd dummy_ca
+./build.sh
+```
+Yep, that easy!
+
+## Credits
+Reading [Jamie's Blog](https://jamielinux.com/docs/openssl-certificate-authority/) is a must!
+
+Made with ♥️ by [kintoandar](https://blog.kintoandar.com)
---
+++ pre_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+: "${ca_pas:?Need to set ca_pas non-empty}"
+: "${countryName:?Need to set countryName non-empty}"
+: "${stateOrProvinceName:?Need to set stateOrProvinceName non-empty}"
+: "${localityName:?Need to set localityName non-empty}"
+: "${organizationName:?Need to set organizationName non-empty}"
+: "${organizationalUnitName:?Need to set organizationalUnitName non-empty}"
+: "${commonName:?Need to set commonName non-empty}"
+: "${emailAddress:?Need to set emailAddress non-empty}"
+: "${crl_addr:?Need to set crl_addr non-empty}"
+: "${crl_port:?Need to set crl_port non-empty}"
+: "${pki_root:?Need to set pki_root non-empty}"
+
+./build.sh \
+  && cat $PWD/pki/intermediate/certs/intermediate.pem > /tmp/vault/ca_bundle.pem \
+  && openssl rsa -passin env:ca_pas -in $PWD/pki/intermediate/private/intermediate.key >> /tmp/vault/ca_bundle.pem
